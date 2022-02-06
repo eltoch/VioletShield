da34ecbd35a123df7c3123d1b2ba4a1e24830c12
MatthewRajan13 <89418442+MatthewRajan13@users.noreply.github.com>
2022-02-06 04:06:25 -0500

    changed to accommodate for bot commands

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 # import libraries
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
 import discord
 import asyncio
 import os
@@ -11,90 +12,116 @@ import os
 # Bot Token
 load_dotenv('C:/Users/matth/Violet Shield/.env')
 TOKEN = os.environ['TOKEN']
+bot = commands.Bot(command_prefix = "!", case_insensitive = True)
 client = discord.Client()
 
-# Banned word levels
-heavy = {'anal', 'anus', 'arse', 'ass', 'smack my ass', 'ballsack', 'balls', 'blowjob', 'blow job', 'bollock', 'boner', 'boob','boobies', 'booby', 'cunt',  'butt', 
-'buttplug', 'clitoris', ' cock', 'dick', 'diddle', 'dildo', 'feck', 'fuck', 'flange', 'jizz', 'knobend', 'penis', 'prick', 'pussy', 'sex', 'shit', 'tit', 'titty', 
-'tittie', 'twat', 'vagina', 'wank', 'whore', 'little boy', 'little girl', 'masturbate', 'jack off', 'kitty', 'kitten', 'porn', 
-'sex', 'horny', 'sexy', 'h0rny', 's3x', 'seggsy', 's3ggsy', 'im hard', 'cum', 'daddy', 'princess', 'suck', 'slut', 'swallow', 'chode', 'nipple', 'lick', 'vag', 
-'virgin', 'tight', 'wet', 'semen', 'you made me cum', 'you make me cum' 'how old are you', 'pegging', 'facial', 'buttfuck', 'butt fuck', 'but tfuck', 'little', 'kid'}
-
-medium = {'Anal', 'smack my ass', 'ballsack', 'balls', 'blowjob', 'blow job', 'boner', 'boob', 'boobies', 'booby', 'cunt', 'buttplug', 'clitoris', 'cock', 'dildo', 'fuck', 
-'jizz', 'knobend', 'penis', 'prick', 'pussy', 'sex','tit', 'titty', 'tittie', 'twat', 'vagina', 'wank', 'whore', 'little boy', 'little girl', 'masturbate', 'jack off', 'kitty', 
-'kitten', 'porn', 'sex', 'horny', 'sexy', 'h0rny', 's3x', 'seggsy', 's3ggsy', 'im hard', 'cum', 'daddy', 'princess', 'suck', 'slut', 'swallow', 
-'chode', 'nipple', 'lick', 'vag', 'virgin', 'tight', 'wet', 'semen', 'you made me cum', 'you make me cum' 'how old are you', 'pegging', 'facial','kid'}
-
-light = {'horny', 'blowjob', 'boner', 'little girl', 'kitten', 'kitty', 'masturbate', 'porn', 'im hard', 'daddy', 'cum', 'princess', 
-'slut', 'you make me hard', 'how old are you', 'pegging', 'peg','kid'}
+heavy = set(())
+medium = set(())
+light = set(())
 
 global banned
 banned = {}
 
+f = open("light.txt", "r") 
+for line in f:
+    line = str(f.readline().rstrip('\n'))
+    light.add(line)
+f.close()
+f = open("medium.txt", "r")
+for line in f:
+    line = str(f.readline().rstrip('\n'))
+    medium.add(line)
+f.close()
+f = open("heavy.txt", "r")
+for line in f:
+    line = str(f.readline().rstrip('\n'))
+    heavy.add(line)
+f.close()
+
 global channel_id
 channel_id = 0
+
+global send
+send = False
 # message read 
 
-#@client.command(pass_context=True)
+
+
+@bot.command(name = 'elevel_heavy')
+@has_permissions(administrator = True) 
+async def elevel_heavy(ctx):
+    global banned
+    banned = heavy
+    await ctx.channel.send('Heavy mode selected')
 
 
 
+@bot.command(name = 'elevel_medium')
+@has_permissions(administrator = True) 
+async def elevel_medium(ctx):
+    global banned
+    banned = medium
+    await ctx.channel.send('Heavy mode selected')
 
-@client.event
+@bot.command(name = 'elevel_light')
+@has_permissions(administrator = True) 
+async def elevel_light(ctx):
+    global banned
+    banned = light
+    await ctx.channel.send('Light mode selected')
+
+@bot.command(name = 'channel_id')
+@has_permissions(administrator = True) 
+async def choose_channel_id(ctx, cid):
+    channel_id = cid.lower()
+    channel_id = cid.replace('o','0')
+    channel_id = cid.replace('l','1')
+    channel_id = cid.replace('i','1')
+    await ctx.channel.send('Channel Name assigned!')
+
+@bot.event
 async def on_message(message):
+
+    global banned
+    global send
 
     msg = message.content.lower()
     msg = msg.replace('@', 'a')
     msg = msg.replace('$', 's')
     msg = msg.replace('\'', '')
     msg = msg.replace('+', 't')
-    msg = msg.replace('0', 'o')
+    #msg = msg.replace('0', 'o')
     msg = msg.replace('1', 'i')
     mgs = msg.replace('_', '')
     mgs = msg.replace('-', '')
 
-    global banned
-    global channel_name
-
-    if (message.content == "!elevel_heavy") and message.author.guild_permissions.administrator:
-        banned = heavy
-        await message.channel.send('Heavy mode selected')
-    elif (message.content == "!elevel_medium") and message.author.guild_permissions.administrator:
-        banned = medium
-        await message.channel.send('Medium mode selected')
-    elif (message.content == "!elevel_light") and message.author.guild_permissions.administrator:
-        banned = light
-        await message.channel.send('Light mode selected')
-    elif (message.content.startswith("!channel_name")) and message.author.guild_permissions.administrator:
-        channel_name = message.content.lstrip("!channel_name ")
-        print(channel_name)
-        await message.channel.send('Channel Name assigned!')
+    print(msg)
+   
+    await bot.process_commands(message)
 
     for word in banned:
 
         # prevents the bot from replying to itself
-        if message.author == client.user:
-            return
+        
+
+        #if message.author == client.user:
+        #    return
 
         if msg.__contains__(word):
             response = '{0.author.mention} message has been blocked. Please be respectful'.format(message)
             await message.channel.send(response)
-
-            
-
             await message.delete()
-            break
+            send  = True
 
-            
+@bot.command(name = 'send')
+@has_permissions(administrator = True)
+async def embed(ctx):
+    global channel_id
+    print('hi')
+    channel = bot.get_channel(channel_id)
+    embed = discord.Embed(title="Embed test", description="A test for my discord bot", color=0x5bcdee)
+    embed.add_field(name="Hello!", value="Hello World!", inline=False)
+    await channel.send(embed=embed)
 
 
-
-# Bot Login
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-client.run(TOKEN)
+bot.run(TOKEN)

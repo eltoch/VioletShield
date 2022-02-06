76d43a32b4a69c219b61f6f27031e4159b92f128
MatthewRajan13 <89418442+MatthewRajan13@users.noreply.github.com>
2022-02-05 20:11:01 -0500

    Allowed for .env use

--- bot.py
+++ bot.py
@@ -12,47 +12,21 @@ TOKEN = os.environ['TOKEN']
 client = discord.Client()
 
 # Banned word levels
-heavy = {'anal', 'anus', 'arse', 'ass', 'smack my ass', 'ballsack', 'balls', 'blowjob', 'blow job', 'bollock', 'boner', 'boob','boobies', 'booby', 'cunt',  'butt', 
-'buttplug', 'clitoris', ' cock', 'dick', 'diddle', 'dildo', 'feck', 'fuck', 'flange', 'jizz', 'knobend', 'penis', 'prick', 'pussy', 'sex', 'shit', 'sh1t', 'tit', 'titty', 
-'tittie', 'twat', 'vagina', 'wank', 'whore', 'little boy', 'little girl', 'masturbate', 'jack off', 'kitty', 'kitten', 'porn', 
-'sex', 'horny', 'sexy', 'h0rny', 's3x', 'seggsy', 's3ggsy', 'im hard', 'cum', 'daddy', 'princess', 'suck', 'slut', 'swallow', 'chode', 'nipple', 'lick', 'vag', 
-'virgin', 'tight', 'wet', 'semen', 'you made me cum', 'you make me cum' 'how old are you', 'pegging', 'facial', 'buttfuck', 'butt fuck', 'but tfuck', 'little'}
-medium = {'Anal', 'smack my ass', 'ballsack', 'balls', 'blowjob', 'blow job', 'boner', 'boob', 'boobies', 'booby', 'cunt', 'buttplug', 'clitoris', 'cock', 'dildo', 'fuck', 
-'jizz', 'knobend', 'penis', 'prick', 'pussy', 'sex','tit', 'titty', 'tittie', 'twat', 'vagina', 'wank', 'whore', 'little boy', 'little girl', 'masturbate', 'jack off', 'kitty', 
-'kitten', 'porn', 'sex', 'horny', 'sexy', 'h0rny', 's3x', 'seggsy', 's3ggsy', 'im hard', 'cum', 'daddy', 'princess', 'suck', 'slut', 'swallow', 
-'chode', 'nipple', 'lick', 'vag', 'virgin', 'tight', 'wet', 'semen', 'you made me cum', 'you make me cum' 'how old are you', 'pegging', 'facial'}
-light = {'horny', 'blowjob', 'boner', 'little girl', 'kitten', 'kitty', 'masturbate', 'porn', 'im hard', 'im hard', 'daddy', 'cum', 'princess', 
-'slut', 'you make me hard', 'you make me cum', 'you made me cum', 'how old are you', 'pegging', 'peg'}
-global banned
-banned = {}
+heavy = ['sex', 'horny', 'sexy', 'h0rny', 's3x', 'seggsy', 's3ggsy', 'I\'m hard']
+medium = ['boner', '']
+light = []
+
 
 @client.event
 async def on_message(message):
-    msg = message.content.lower()
+    # prevents the bot from replying to itself
+    if message.author == client.user:
+        return
 
-    global banned
-
-    if message.content == "!elevel_heavy":
-        banned = heavy
-        await message.channel.send('Heavy mode selected')
-    elif message.content == "!elevel_medium":
-        banned = medium
-        await message.channel.send('Medium mode selected')
-    elif message.content == "!elevel_light":
-        banned = light
-        await message.channel.send('Light mode selected')
-    
-    for word in banned:
-
-        # prevents the bot from replying to itself
-        if message.author == client.user:
-            return
-
-        if msg.__contains__(word):
-            response = 'Hello {0.author.mention} pls no horny'.format(message)
-            # await message.channel.send(msg)
-            await message.channel.send(response)
-            
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        # await message.channel.send(msg)
+        await message.channel.send(heavy[3])
 
 
 
